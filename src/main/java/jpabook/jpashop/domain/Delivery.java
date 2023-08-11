22f5f4c0d04296eb05aa7029947dc8f160de900d
josh <tkddns6007@gmail.com>
2023-08-11 13:44:46 +0900

    domain entity 생성

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -13,12 +13,12 @@ public class Delivery {
     @Column(name = "delivery_id")
     private Long id;
 
-    @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
+    @OneToOne(mappedBy = "delivery")
     private Order order;
 
     @Embedded
     private Address address;
 
     @Enumerated(EnumType.STRING)
-    private DeliveryStatus status; //ENUM [READY(준비), COMP(배송)]
+    private DeliveryStatus status; //READY, COMP
 }
--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,17 +1,13 @@
 package jpabook.jpashop.domain.item;
 
-import jpabook.jpashop.domain.Category;
-import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "dtype")
+@DiscriminatorColumn
 @Getter @Setter
 public abstract class Item {
 
@@ -19,22 +15,8 @@ public abstract class Item {
     @GeneratedValue
     @Column(name = "item_id")
     private Long id;
+
     private String name;
     private int price;
     private int stockQuantity;
-    @ManyToMany(mappedBy = "items")
-    private List<Category> categories = new ArrayList<Category>();
-
-    // == 비즈니스 로직 == //
-    public void addStock(int quantity) {
-        this.stockQuantity += quantity;
-    }
-
-    public void removeStock(int quantity) {
-        int restStock = this.stockQuantity - quantity;
-        if (restStock < 0) {
-            throw new NotEnoughStockException("need more stock");
-        }
-        this.stockQuantity = restStock;
-    }
 }
